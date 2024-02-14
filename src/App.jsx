7d071a53f8b09fefc5488196e25b9df7de9a1aab
danihermes <danielhn@gmail.com>
2024-02-14 07:35:51 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Portfolio from "./components/Portfolio"
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <h1>Welcome to My Portfolio Website</h1>
+      </header>
+      <main>
+        <Portfolio />
+      </main>
+      <footer>
+        {/* Add your footer content here */}
+      </footer>
+    </>
+  )
+}
+
+export default App
