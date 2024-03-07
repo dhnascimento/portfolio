34b15bc7881ec67c423d4e655be2b4e3c0831988
danihermes <danielhn@gmail.com>
2024-03-07 18:10:24 -0500

    update imports for components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import Navbar from "./components/Navbar";
-import About from "./components/About/About";
+import Header from "./components/Header";
+import Hero from "./components/Hero/Hero";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import Contact from "./components/Contact";
+import About from "./components/About";
+
 
 function App() {
   return (
-    <main>
-      <Navbar />
-      <About />
+    <main className="bg-gray-900">
+      <Header />
+      <Hero />
+      <About/>
       <Projects />
       <Skills />
       <Contact />
---
+++ src/helpers/tailwindCommon.js
@@ -0,0 +1,8 @@
+const tailwindCommon = {
+    h2: 'text-6xl text-white text-center mb-4',
+    h3: 'text-white text-4xl mb-6',
+    p_about: 'text-white text-left mb-2',
+    skill_badge: 'rounded-lg bg-stone-300 p-2 m-3'
+}
+
+export default tailwindCommon;
